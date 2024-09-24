8004b651a44422d9bcce77a0891af261b6b22421
anapereira153 <ana_pereira153@estudante.sesisenai.org.br>
2024-09-24 18:10:20 -0300

    Exercícios de fixação

--- script.js
+++ script.js
@@ -1,55 +1,108 @@
-function Executar1008(){
-    let id, horas, valorHora, salario
-    //let horas 
-    //let valorHora
-    //let salario
-    id =  Number(prompt("Digite seu id: "))
-   // id = Number('45')
-   // id = 45
-   horas = Number(prompt("Quantas horas você trabalhou?"))
-   valorHora = Number(prompt("Quantos você ganha por hora?"))
-   salario = horas * valorHora
-   alert("ID: " + "\nSalário: R$" + salario.toFixed(2))
-}
-
-function Executar1009(){
-    let salarioFixo, comissão, vendedor, totaldeVendas, salariocombonus
-    vendedor = (prompt("Digite o nome do vendedor"))
-    salarioFixo = Number(prompt("Digite o salario Fixo"))
-    totaldeVendas = Number(prompt("Digite o total de vendas"))
-    comissão = totaldeVendas * 0.15
-    salariocombonus = salarioFixo + comissão
-    
-    alert("vendedor" + vendedor+ "\n salario com bonus R$" + salariocombonus.toFixed(2))
-
-}
-
-
-function simularCorrida(){
-    let distancia = Number (prompt("Qual a distancia desejada?"))
-    let distanciaX = 0;
-    let distaciaY = 0;
-    tempo = 0
-
-    while(distanciaY-distanciaX < distancia){
-        distanciaX +=1
-        distanciaY +=1.5
-        tempo++
-        console.log()
+function atv1(){
+    let n = Number(prompt("Digite um número!")); //entrada manual
+    let i = 1;                                 //processo
+    while(i <= n){                            //laço de repetiçao
+        alert(i);                            //Display
+        i ++;                               //processo
     }
 }
 
-function Executar1038(){
-    //ler a quntidade
-    let quantidade = Number(prompt("Quantidade:"))
- // definir o preço do produtp
- let preço
- if(codigo == 1){
-     preço = 4
- }else if(codigo == 2){
-    preço = 4.5
- }else if(codigo == 3){
-
- }
- // calcular o valor da conta  (preço * quantidade)
+function atv2(){
+    let n = Number(prompt("Digite um número!")); //Entrada manual
+    let soma = 0;
+    while(n != 0){
+        soma = soma + n;
+        n = Number(prompt("Digite um número!"));
+    }
+    alert(soma);
 }
+
+function atv3(){
+    let qtdTotal = Number(prompt("Digite a quantidade de número!")) ;
+    let soma = 0;
+    for(let qtdcad = 0; qtdcad < qtdTotal; qtdcad ++){
+        let número = Number(prompt("Digite o número"));
+        soma = soma + número;
+        qtdcad ++;
+    }
+    let media = soma/qtdTotal;
+    alert(media);
+   }
+   
+   function atv4(){
+       let n = Number(prompt("Digite o número para calcular o fatorial!"))
+       let r = 1;
+       for(let i = 1; i <= n; i ++){
+       }
+       alert(r);
+   }
+
+   function atv5(){
+    let n = Number(prompt("Digite um número!"));
+    let tabela = "";
+    //for(inicialização; condição; incremento){trecho que será repetido}
+    for(let mult = 1; mult <=10; mult ++){
+        tabela += "\n"+n+" x "+mult+" = "+(n*mult);
+    }
+    alert(tabela);
+   }
+
+   function atv6(){
+    let n = Number(prompt("Digite um número!"));
+    let pares = "";
+    for(let par = 1; par <= n; par ++){
+        if(par % 2 == 0){
+            pares += "\n"+par;
+        }
+        alert(pares)
+    }
+   }
+
+   function atv7(){
+    let n = Number(prompt("Digite um número!"));
+    let passo = Number(prompt("Digite um passo!"));
+    let i = 1;
+    while(i < n){
+        alert(i);
+        i+= passo;
+    }
+   }
+
+   function atv8(){
+    let n = Number(prompt("Digite um número!"));
+    let impares = "";
+    for(let i = 1; i <= n; i ++){
+        if(i % 2 !== 0){
+            impares += "\n"+i;
+        }
+        alert(impares);
+    }
+   }
+
+   function atv9(){
+    let n1 = Number(prompt("Digite um número!"));
+    let n2 = Number(prompt("Digite o possivel multiplo!"));
+    if(n2 % n1 == 0){
+        alert(n2+" é multiplo de "+n1);
+    }
+    else{
+        alert(n2+" não é multiplo de "+n1);
+    }
+   }
+
+   function atv10(){
+    let n = Number(prompt("Digite um número!"));
+    let qtdDivisores = 0;
+    let ant = 1;
+    while(ant <= n){
+        if(n % ant == 0){
+            qtdDivisores ++;
+        }
+        ant ++;
+    }
+    if(qtdDivisores == 2){
+        alert(n +" é primo!");
+    }else{
+        alert(n +"não é primo!");
+    }
+   }
